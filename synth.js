684a022d9e3d85f24ce2c0124aa0780e25d6d5e9
poushitaguha <pguha.kolkata@gmail.com>
2019-10-11 00:05:27 -0400

    Add audiokeys

--- synth.js
+++ synth.js
@@ -1,1 +1,25 @@
 console.log("hello there!!")
+
+import AudioKeys from "audiokeys";
+
+let ctx = new AudioContext();
+
+// create a keyboard
+var keyboard = new AudioKeys();
+
+let oscMap = {};
+
+keyboard.down(function (note) {
+    let osc = ctx.createOscillator();
+    osc.frequency.value = note.frequency;
+    osc.connect(ctx.destination);
+    osc.start();
+
+    oscMap[note.frequency] = osc;
+});
+
+keyboard.up(function (note) {
+    let osc = oscMap[note.frequency];
+    osc.stop();
+    // do things with the note object
+});
